30b623130f9d8cf9ba4e7c1480cf419557adc1c0
Kejvin Braka <kejvin.braka@atis.al>
2025-07-24 09:59:53 +0200

    change

--- index.js
+++ index.js
@@ -1,24 +1,74 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
+const axios = require("axios");
+const dns = require("dns");
+const { URL } = require("url");
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", function (req, res) {
+  const { url } = req.body;
+  let hostname;
+
+  // Step 1: Validate URL format and protocol
+  try {
+    const parsedUrl = new URL(url);
+    if (parsedUrl.protocol !== "http:" && parsedUrl.protocol !== "https:") {
+      return res.json({ error: "invalid url" });
+    }
+    hostname = parsedUrl.hostname;
+  } catch (err) {
+    return res.json({ error: "invalid url" });
+  }
+
+  dns.lookup(hostname, (err) => {
+    if (err) {
+      return res.json({ error: "invalid url" });
+    }
+
+    const shortUrl = generateShortUrl();
+    axios
+      .post("http://localhost:4000/shorturl", {
+        short_url: shortUrl,
+        original_url: url,
+      })
+      .then((axiosRes) => {
+        res.json(axiosRes.data);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Failed to save short URL" });
+      });
+  });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:shortId", (req, res) => {
+  axios.get(`http://localhost:4000/shorturl?short_url=${req.params.shortId}`).then((result) => {
+    if (result.data[0]) {
+      res.redirect(result.data[0].original_url);
+    } else {
+      res.send("Could not find shortened url.");
+    }
+  });
+});
+
+function generateShortUrl() {
+  return crypto.randomBytes(12).toString("base64url");
+}
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
